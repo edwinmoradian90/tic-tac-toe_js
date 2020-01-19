b42177cf7bdfa48cd0a669abe457e52ecd15f32c
Edwin Moradian <edwin.moradian90@gmail.com>
2020-01-18 16:16:17 -0800

    added player swapping

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -15,23 +15,27 @@ const Gameboard = (function(){
         gameboard.addEventListener('click', (e) => {
             let move = e.target.dataset.space;
             if(e.target.matches('.space')){
-                Game.move(move);
-                Game.swapPlayer();
+                Game.update(move);
             };
         });
     };
+    
+    const isFull = function(){
+        return this.gameboard.includes('');
+    }
 
     const emptySpace = function(move) {
-        return gameboard[move] == '';
+        return this.gameboard[move] == '';
     };
 
-    const addMoveToGameboardArray = function(move, isEmpty){
-        gameboard[move] = (isEmpty) 
+    const addMoveToGameboardArray = function(move, spaceIsEmpty){
+        this.gameboard[move] = (spaceIsEmpty) 
             ? Game.current.piece
             : console.log('space not empty');
     };
 
     return {
+        isFull,
         gameboard,
         emptySpace,
         initialize,
@@ -41,8 +45,8 @@ const Gameboard = (function(){
 
 const Game = (function(){
     let players = [
-        Player('', 'x', '1'), 
-        Player('','o','2'),
+        Player('', 'x', 1), 
+        Player('','o', 2),
     ];
     let current = '';
 
@@ -62,17 +66,37 @@ const Game = (function(){
     };
 
     const swapPlayer = function() {
-       current = current.id = 1 
-       ? players[1] 
-       : players[0];
+        this.current = (this.current.id == 1) 
+            ? players[1] 
+            : players[0];
     }
 
-    const move = function(move){
-        const isEmpty = Gameboard.emptySpace(move);
-        console.log(isEmpty)
-        Gameboard.addMoveToGameboardArray(move, isEmpty);
-        View.addMoveToGameboard(move, isEmpty);
-        swapPlayer();
+    const move = function(move) {
+        const spaceIsEmpty = Gameboard.emptySpace(move);
+        console.log(spaceIsEmpty)
+        Gameboard.addMoveToGameboardArray(move, spaceIsEmpty);
+        View.addMoveToGameboard(move, spaceIsEmpty);
+    }
+
+    const _draw = function() {
+        
+    }
+
+    const endGame = function() {
+        
+    }
+
+    const update = function(move) {
+        const boardIsFull = Gameboard.isFull();
+        if(boardIsFull){
+            this._draw();
+        } 
+        else {
+            this.move(move);
+            this.current.turn += 1;
+            console.log(this.current.turn)
+            this.swapPlayer();
+        };
     }
 
     return {
@@ -81,13 +105,14 @@ const Game = (function(){
         current,
         swapPlayer,
         move,
+        update,
     };
 })();
 
 const View = (function(){
 
-    const addMoveToGameboard = function(move, isEmpty){
-        if(isEmpty){
+    const addMoveToGameboard = function(move, spaceIsEmpty){
+        if(spaceIsEmpty){
             const space = 
                 document.querySelector( `[data-space='${move}']` );
             const piece = Game.current.piece;
